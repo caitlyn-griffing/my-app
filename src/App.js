9d8e4c4e2cafc2904d7f90047a781e642275f331
Neil Juanillo <37945822+ajax12233512@users.noreply.github.com>
2022-03-04 13:26:04 -0600

    project-template section layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Project from './components/Project/Project';
 
 function App() {
   return (
     <>
     <Router>
       <Navbar />
+      <Project />
+      <Project />
+      <Project />
       {/* <Switch>
         <Route path='/' exact />
       </Switch> */}
---
+++ src/components/Project/Project.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Project.css'
+
+
+function Project() {
+  return (
+    <div>
+        <img alt='project'/>
+        <h2>Title</h2> 
+        <p>
+            Descriptsion <br/>
+            Descriptsion <br/>
+            Descriptsion <br/>
+            Descriptsion <br/>
+            Descriptsion <br/>
+        </p>
+        <ul>
+            <li><a href='/'>Ecommerce</a></li>
+            <li><a href='/'>Other</a></li>
+            <li><a href='/'>Other</a></li>
+            <li><a href='/'>Other</a></li>
+            <li><a href='/'>Other</a></li>
+            <li><a href='/'>Other</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Project
